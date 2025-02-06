8e7fed4d886b6082e462f647c33ed43919fdb421
Abel Yoshuara <abelyoshuara@gmail.com>
2025-02-07 04:30:33 +0800

    solve: string templates bug fixing #5

---
+++ src/string-templates-bug-fixing-#5.js
@@ -0,0 +1,10 @@
+function buildString(...template) {
+  return `I like ${template.join(", ")}!`;
+}
+
+console.log(
+  buildString("Cheese", "Milk", "Chocolate"),
+  "I like Cheese, Milk, Chocolate!",
+);
+console.log(buildString("Cheese", "Milk"), "I like Cheese, Milk!");
+console.log(buildString("Chocolate"), "I like Chocolate!");
